9e966dc809b9f449f50e8ce0969b8989d0c65c8b
ferrari-kaiser <ferrari-kaiser@hotmail.com>
2017-03-11 02:21:36 -0300

    Tela de Cadastro e Fluxo da mesma

--- InMaisAndroid/app/src/main/java/itspay/br/com/activity/CadastroEnderecoAcitvity.java
+++ InMaisAndroid/app/src/main/java/itspay/br/com/activity/CadastroEnderecoAcitvity.java
@@ -7,11 +7,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import itspay.br.com.itspay.R;
 
 
 public class CadastroEnderecoAcitvity extends AppCompatActivity {
+
+    Spinner mSpCidade;
+    Spinner mSpEstado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,32 @@ public class CadastroEnderecoAcitvity extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro_endereco_acitvity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Cidade
+        mSpCidade = (Spinner) findViewById(R.id.sp_cidade);
+
+        List<String> cidadeList = new ArrayList<String>();
+        cidadeList.add("Brasília");
+        cidadeList.add("Uberlândia");
+        cidadeList.add("Porto Alegre");
+
+        ArrayAdapter<String> cidadeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, cidadeList);
+        cidadeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpCidade.setAdapter(cidadeAdapter);
+
+
+        // Estado
+        mSpEstado = (Spinner) findViewById(R.id.sp_estado);
+
+        List<String> estadoList = new ArrayList<String>();
+        estadoList.add("Rio Grande do Sul");
+        estadoList.add("Distrito Federal");
+        estadoList.add("Rio de Janeiro");
+
+        ArrayAdapter<String> estadoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, estadoList);
+        estadoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpEstado.setAdapter(estadoAdapter);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
